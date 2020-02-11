5172d75c3f680ccde066bf3d40c9141ed86d7fd1
Will Norton <norton402@morrisville.edu>
2020-02-10 20:04:02 -0500

    finished regions, start on code

---
+++ Assets/Scripts/Infection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EAB.Game
+{
+    public class Infection : MonoBehaviour
+    {
+        //initialize variables 
+        int startingRegion = 0;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        bool StartingRegionPicked()
+        {
+            if (startingRegion != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
