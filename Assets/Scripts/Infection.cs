7eb62fe528f7fe1868e4a4348adc6ecc658feea2
Will Norton <norton402@morrisville.edu>
2020-02-12 14:36:08 -0500

    buttons call methods

---
+++ Assets/Scripts/Infection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infection : MonoBehaviour
+{
+    //int variable keeping track of the starting region 
+    int startingRegion = 0;
+
+    //int variables with values for each region
+    int northernRegion = 1;
+    int westernRegion = 2;
+    int centralRegion = 3;
+    int southernRegion = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //methods to set starting region and disable buttons 
+    //TODO disable buttons 
+    public void SetNorthernStartingRegion()
+    {
+        startingRegion = northernRegion;
+    }
+    public void SetWesternStartingRegion()
+    {
+        startingRegion = westernRegion;
+    }
+    public void SetCentralStartingRegion()
+    {
+        startingRegion = centralRegion;
+    }
+    public void SetSouthernStartingRegion()
+    {
+        startingRegion = southernRegion;
+    }
+
+}
