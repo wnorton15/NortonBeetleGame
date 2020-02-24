0276d8b0e65036177b4171239975097a9cee9698
Will Norton <norton402@morrisville.edu>
2020-02-24 10:01:30 -0500

    more work on upgrades

--- Assets/Scripts/Upgrades.cs
+++ Assets/Scripts/Upgrades.cs
@@ -22,8 +22,19 @@ public class Upgrades : MonoBehaviour
 
     public void OpenUpgradeScreen()
     {
+        foreach (GameObject layer in otherLayersOpen)
+        {
+            layer.SetActive(false);
+        }
         upgradeMenu.SetActive(true);
     }
 
-
+    public void backToGame()
+    {
+        upgradeMenu.SetActive(false);
+        foreach (GameObject layer in otherLayersOpen)
+        {
+            layer.SetActive(true);
+        }
+    }
 }
