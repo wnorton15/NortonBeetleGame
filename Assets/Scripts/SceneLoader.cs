b2b3e50c75cf9e966f3ae9b2670b357babb5ce6e
Will Norton <norton402@morrisville.edu>
2020-02-28 14:09:05 -0500

    added sounds and win screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace beetle.SceneManagement
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        //function to load the next scene
+        public void LoadNextScene()
+        {
+            //creates the scene index as an int
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            //calls the next active scene
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+
+        //function to quit the game
+        public void quitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
